33d1cc9fb0e5b73836145db85465bcca41a3b3b4
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-04-28 14:26:14 -0500

    added workout schema to models folder

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        //set default to current date.
+        default: Date.now,
+    },
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                
+            },
+
+            name: {
+                type: String,
+            },
+
+            duration: {
+                type: Number,
+            },
+
+            weight: {
+                type: Number,
+            },
+
+            reps: {
+                type: Number,
+            },
+
+            sets: {
+                type: Number,
+            },
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
